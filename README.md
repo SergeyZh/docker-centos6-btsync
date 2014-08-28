1080240c87af467fd7530f8457796e1cab2b9493
Andrey Sizov <andrey.sizov@jetbrains.com>
2014-08-28 17:31:19 +0400

    Added r/o secret key generator. Modified README.

--- README.md
+++ README.md
@@ -10,4 +10,6 @@ docker-centos6-btsync
 * `USE_DHT` - `true` or `false` enable or disable DHT. Default `false`
 
 You may set environment variable BTSYNC_USERID to run btsync under other Unix account.
-It may be necessary to access files under /mnt/storage.
+If you want to add your data - place it under `/mnt/storage/` folder. To add
+multiple folders you could add it as volumes: `-v /your/data/folder1:/mnt/storage/folder1 -v
+/your/data/folder2:/mnt/storage/folder2`.
--- run-services.sh
+++ run-services.sh
@@ -6,7 +6,9 @@ NODENAME=`hostname -s`
 if [ -z "${STORAGE_SECRET}" ] ; then
     echo "Can't run without environment variable STORAGE_SECRET !"
     TMP_VAL=`/btsync --generate-secret`
-    echo "You can use the following generated value: "${TMP_VAL}" ."
+    RO_KEY=`/btsync --get-ro-secret ${TMP_VAL}`
+    echo "You can use the following generated value: ${TMP_VAL}"
+    echo "Read-only secret from that generated secret is: ${RO_KEY}" 
     echo "Just add the following parameter to your \"docker run\" command : -e STORAGE_SECRET="${TMP_VAL}
     echo "Exiting..."
     exit 1
